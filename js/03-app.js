bba499f32b4af410f4bc22c131cbfb3c6ab94477
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-07-05 19:27:01 +0200

    Adds the clear method to clear the storaged info.

--- js/03-app.js
+++ js/03-app.js
@@ -6,3 +6,7 @@ const mesesArray = JSON.parse(localStorage.getItem('meses'));
 console.log(mesesArray);
 
 mesesArray.push('Nuevo mes');
+console.log(mesesArray);
+localStorage.setItem('meses', JSON.stringify(mesesArray));
+
+localStorage.clear();
